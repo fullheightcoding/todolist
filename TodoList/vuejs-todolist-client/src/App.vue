0e0eb8b4c5fe9db99c85b6473c608a6c7939f661
fullheightcoding <mark.dodgson.developer@gmail.com>
2020-10-03 12:29:41 +1000

    Create project using vue cli to refresh

--- TodoList/vuejs-todolist-client/src/App.vue
+++ TodoList/vuejs-todolist-client/src/App.vue
@@ -1,64 +1,28 @@
 <template>
   <div id="app">
-    <!-- <Home msg="Welcome to Your Vue.js App"/> -->
-
-    <!-- Bootstrap nav -->
-   <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <!-- <a class="navbar-brand" href="#">TodoList</a> -->
-          <router-link to="/home" class="navbar-brand">TodoList</router-link>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <!-- use router-link component for navigation. -->
-            <li><router-link to="/contact">Contact</router-link></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
-    <div class="container">
-    <!-- route outlet -->
-    <router-view></router-view>
-    </div>
-
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Contact from './components/Contact.vue'
-import Home from './components/Home.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    Contact,
-    Home,
+    HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-[v-cloak] {
-    display: none;
-}
-.starter-template {
-  padding: 40px 15px;
-  text-align: center;
 }
 </style>
